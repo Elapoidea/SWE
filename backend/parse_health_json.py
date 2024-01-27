6f77faa6f761f90409ad7bd495bfca7533118759
Arian Baishya <arian.baishya2004@gmail.com>
2024-01-27 12:55:02 -0600

    parsing stuff

--- backend/parse_health_json.py
+++ backend/parse_health_json.py
@@ -2,10 +2,8 @@ from dataclasses import dataclass
 from datetime import date, datetime
 import json
 
-
 @dataclass
 class SensorData:
-    date: datetime
     bpm: int
     weight: int
     exercise_hours: int
@@ -13,42 +11,37 @@ class SensorData:
 
 @dataclass
 class HealthConcern:
-    date: date
     concern: str
 
 
 @dataclass
 class HealthData:
-    sensor_datas: list[SensorData]
-    health_concerns: list[HealthConcern]
+    sensor_datas: dict[datetime, SensorData]
+    health_concerns: dict[date, HealthConcern]
 
     @staticmethod
-    def make_from_json(file_path: str) -> "HealthData":
+    def make_from_json(data: dict) -> "HealthData":
         # For expected format, refer to json_example.jsonc
-        with open(file_path) as f:
-            data = json.load(f)
+        sensor_datas = []
+        for date_str, sensor_data in data["sensor_datas"].items():
+            sensor_date = datetime.strptime(date_str, "%d/%m/%y")
 
-            sensor_datas = []
-            for date_str, datas in data["sensor_datas"].items():
-                sensor_date = datetime.strptime(date_str, "%d/%m/%y")
+            sensor_datas[sensor_date, SensorData(sensor_data.value())]
 
-                s = SensorData(sensor_date, *datas.values())
-                sensor_datas.append(s)
+        # health_concerns = []
+        # for date_str, concern in data["health_concerns"].items():
+        #     concern_date = datetime.strptime(date_str, "%d/%m/%y").date()
+        #
+        #     hc = HealthConcern(concern_date, concern)
+        #     health_concerns.append(hc)
+        #
+        return HealthData(sensor_datas, health_concerns)
 
-            health_concerns = []
-            for date_str, concern in data["health_concerns"].items():
-                concern_date = datetime.strptime(date_str, "%d/%m/%y").date()
-
-                hc = HealthConcern(concern_date, concern)
-                health_concerns.append(hc)
-
-            return HealthData(sensor_datas, health_concerns)
-
-
-def main():
-    hd = HealthData.make_from_json("./json_example.json")
-    print(hd)
+    # Accessors
+    def get_sensor_data_range(self, start_date: datetime, end_date: datetime) -> dict:
+        data_from_range = {}
+        # TODO: get data within range
+        return data_from_range
 
 
-if __name__ == "__main__":
-    main()
+    # Remove methods
