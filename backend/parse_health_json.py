1ca92468ae9b77345d7987ad81d37a1703b1b8e0
Joshua Purushothaman <52509691+joshuaPurushothaman@users.noreply.github.com>
2024-01-27 11:55:24 -0600

    beautiful statically typed parse_health_json. i <3 dataclasses

--- backend/parse_health_json.py
+++ backend/parse_health_json.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import datetime
+from datetime import date, datetime
 import json
 
 
@@ -11,31 +11,42 @@ class SensorData:
 
 
 @dataclass
-class HealthConcerns:
-    concerns: dict[datetime.datetime, str]
-
-    def get_by_date(self, date: datetime.datetime) -> str:
-        return self.concerns[date]
+class HealthConcern:
+    date: date
+    concern: str
 
 
 @dataclass
 class HealthData:
-    sensor_data: SensorData
-    health_concerns: HealthConcerns
+    sensor_datas: list[SensorData]
+    health_concerns: list[HealthConcern]
 
+    @staticmethod
+    def make_from_json(file_path: str) -> "HealthData":
+        # For expected format, refer to json_example.jsonc
+        with open(file_path) as f:
+            data = json.load(f)
 
-def read_health_from_json(file_path: str) -> HealthData:
-    # For expected format, refer to json_example.jsonc
-    with open(file_path) as f:
-        data = json.load(f)
+            sensor_datas = []
+            for date_str, datas in data["sensor_datas"].items():
+                sensor_date = datetime.strptime(date_str, "%d/%m/%y").date()
 
-        s = SensorData(*tuple(data["sensor_data"]))
-        print(s)
-        return None
+                s = SensorData(*datas.values())
+                sensor_datas.append(s)
+
+            health_concerns = []
+            for date_str, concern in data["health_concerns"].items():
+                concern_date = datetime.strptime(date_str, "%d/%m/%y").date()
+
+                hc = HealthConcern(concern_date, concern)
+                health_concerns.append(hc)
+
+            return HealthData(sensor_datas, health_concerns)
 
 
 def main():
-    pass
+    hd = HealthData.make_from_json("./json_example.json")
+    print(hd)
 
 
 if __name__ == "__main__":
